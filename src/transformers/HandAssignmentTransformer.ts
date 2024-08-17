d9d1ad2e21bc013de582943cf149a638f53e0c04
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2024-08-17 16:20:45 +0200

    improve hand assignment transformer

--- src/transformers/HandAssignmentTransformer.ts
+++ src/transformers/HandAssignmentTransformer.ts
@@ -1,132 +1,147 @@
 import { v4 } from "uuid";
 import { Transformer } from "./Transformer";
 import { HandAssignment } from "../types";
-import { AppNode, findAncestor, CollatedEventNode, find, RdgNode } from "./Node";
+import { AppNode, findAncestor, CollatedEventNode, find, RdgNode, AnyRollEventNode, isRollEventNode, filter, wrap } from "./Node";
 
 export class HandAssignmentTransformer extends Transformer<HandAssignment> {
+    /**
+     * Takes out a roll event of its collated context and 
+     * creates a new collated event for it. 
+     * @param affectedNode The roll event to extract
+     * @returns The newly created collated event.
+     * @note The parent of the newly created collated event will
+     * have the same parent as the original event, but it will not
+     * be appended to its parents' children.
+     */
+    extractEventFromCollation(affectedNode: AnyRollEventNode): CollatedEventNode | undefined {
+        const collatedEvent = affectedNode.parent
+        if (collatedEvent.type !== 'collatedEvent') {
+            return
+        }
+
+        const index = collatedEvent.children.findIndex(e => e.xmlId === affectedNode.xmlId)
+        if (index === -1) {
+            throw new Error("Event is not a child of its parent")
+        }
+        collatedEvent.children.splice(index, 1)
+
+        const newCollatedEvent: CollatedEventNode = {
+            type: 'collatedEvent',
+            xmlId: v4(),
+            children: [affectedNode],
+            parent: collatedEvent.parent
+        }
+        affectedNode.parent = newCollatedEvent
+        return newCollatedEvent
+    }
+
+    sortByExistingReading(events: AnyRollEventNode[]): Map<string | null, AnyRollEventNode[]> {
+        const result = new Map<string | null, AnyRollEventNode[]>()
+
+        for (const event of events) {
+            const reading = findAncestor(event, 'rdg')
+            const key = reading?.xmlId || null
+            result.set(key, [...(result.get(key) || []), event])
+        }
+
+        return result
+    }
+
     apply(assumption: HandAssignment) {
-        for (const assignedTo of assumption.assignedTo) {
-            const affectedNode = find(this.body, assignedTo.id)
-            if (!affectedNode) {
-                console.log(`
-                    The roll event ${assignedTo.id} was not found in the tree. 
-                    Make sure that the HandAssignmentTransformer runs after
-                    events have been inserted into the tree.`
-                )
-                continue
-            }
+        if (!assumption.assignedTo.length) {
+            console.log('Empty assumption passed to transformer')
+            return
+        }
 
-            if (affectedNode.type !== 'note' && affectedNode.type !== 'expression') {
-                console.log('Hand was assigned to an unsupported roll event type')
-                continue
-            }
+        const source = this.sourceOf(assumption.assignedTo[0].id)
+        if (!source) {
+            console.log('Source of events to which a hand was assigned cannot be determined')
+            return
+        }
 
-            if (affectedNode.parent.type !== 'collatedEvent') {
-                console.log(`This transformer must not be applied after collated events were unpacked`)
-                continue
-            }
-            const collatedEvent: CollatedEventNode = affectedNode.parent
+        const byReading = this.sortByExistingReading(
+            assumption.assignedTo
+                .map(event => find(this.body, event.id))
+                .filter(event => event !== undefined && isRollEventNode(event)) as AnyRollEventNode[]
+        )
 
-            const rdgNode = findAncestor(affectedNode, 'rdg') as RdgNode | undefined
-            if (rdgNode) {
-                // the roll event is already part of a reading. Take 
-                // it out from the list of sources (unless it consists
-                // only of that source) and separate it into a new reading
-                const affectedSource = this.sourceOf(affectedNode.xmlId)
-                if (!affectedSource) {
-                    console.log('No source could be determined for roll event', affectedNode)
-                    continue
+        for (const [readingId, eventsWithHand] of byReading) {
+            if (readingId === null) {
+                // case 1: the events belong to any reading yet.
+                // Take them out of their collated context and 
+                // put them in their own <rdg> inside a new <app>
+                const app: AppNode = {
+                    parent: this.body,
+                    children: [],
+                    xmlId: v4(),
+                    type: 'app'
                 }
-
-                const sourceIndex = rdgNode.source.findIndex(source => source === affectedSource)
-                if (sourceIndex === -1) {
-                    console.log(
-                        `Roll event is part of a reading node, but its source
-                        is not listed in the readings\' source attribute`
-                    )
-                    continue
-                }
-                rdgNode.source.splice(sourceIndex, 1)
-
-                // Take it out from its original context 
-                const eventIndex = collatedEvent.children.findIndex(e => e.xmlId === affectedNode.xmlId)
-                if (eventIndex === -1) {
-                    throw new Error("Event is not a child of its parent")
-                }
-                collatedEvent.children.splice(eventIndex, 1)
-
-                const appNode = rdgNode.parent
 
                 const newRdg: RdgNode = {
                     type: 'rdg',
+                    hand: [assumption.hand.id],
+                    parent: app,
                     xmlId: assumption.id,
                     children: [],
-                    parent: appNode,
-                    source: [affectedSource]
+                    source: [source]
                 }
 
-                const newCollatedEvent: CollatedEventNode = {
-                    type: 'collatedEvent',
-                    parent: newRdg,
-                    children: [affectedNode],
-                    xmlId: v4(),
-                }
-                affectedNode.parent = newCollatedEvent
-                newRdg.children = [newCollatedEvent]
-                appNode.children.push(newRdg)
+                const newCollatedEvents = eventsWithHand
+                    .map(e => this.extractEventFromCollation(e))
+                    .filter(e => e !== undefined)
+                wrap(newCollatedEvents, newRdg)
+                wrap([newRdg], app)
+
+                this.body.children.push(app)
+                continue
+            }
+
+            const rdg = find(this.body, readingId) as RdgNode | undefined
+            if (!rdg) continue
+
+            // case 2: the events are grouped already in a reading
+            // and all elements in the reading are affected
+            // by the hand assignment => simply add a hand to that 
+            // reading and we are done
+            const existingEvents = filter(rdg, node => isRollEventNode(node))
+            const congruent = existingEvents.every(e =>
+                eventsWithHand.findIndex(other => other === e) !== -1
+            )
+            if (congruent) {
+                rdg.hand = [assumption.hand.id]
+                rdg.xmlId = assumption.id
+                console.log('new rdg', rdg)
             }
             else {
-                // this roll event is not yet marked as a reading. 
-                // Take it out from its original context 
-                const index = collatedEvent.children.findIndex(e => e.xmlId === affectedNode.xmlId)
-                if (index === -1) {
-                    throw new Error("Event is not a child of its parent")
-                }
-                collatedEvent.children.splice(index, 1)
-
-                const appNode: AppNode = {
-                    type: 'app',
-                    xmlId: v4(),
-                    parent: this.body,
-                    children: []
-                }
-
-                const rdgNode: RdgNode = {
+                // case 3: already grouped in a reading, but not all
+                // elements of that group share the same hand
+                // => take "our" events out of their collated context,
+                // append another reading to the existing apparatus 
+                // with the same source, but with the hand attribute set
+                const newRdg: RdgNode = {
                     type: 'rdg',
-                    xmlId: v4(),
-                    parent: appNode,
-                    children: collatedEvent.children.length === 0 ? [collatedEvent] : [],
-                    source: collatedEvent.children.length === 0
-                        ? this.sources.map(source => source.id).filter(source => source !== this.sourceOf(affectedNode.xmlId))
-                        : Array.from(
-                            new Set(
-                                collatedEvent.children.map(e => this.sourceOf(e.xmlId) || 'unknown source')
-                            )
-                        )
-                }
-
-                const otherRdgNode: RdgNode = {
-                    type: 'rdg',
-                    xmlId: v4(),
-                    parent: appNode,
+                    parent: rdg.parent,
+                    hand: [assumption.hand.id],
                     children: [],
-                    source: [this.sourceOf(affectedNode.xmlId) || 'unknown source'],
-                    hand: [assumption.hand.id]
+                    xmlId: assumption.id,
+                    source: [source]
                 }
+                rdg.parent.children.push(newRdg)
 
-                const newCollatedEvent: CollatedEventNode = {
-                    type: 'collatedEvent',
-                    xmlId: v4(),
-                    children: [affectedNode],
-                    parent: otherRdgNode
+                const newCollatedEvents = eventsWithHand
+                    .map(e => this.extractEventFromCollation(e))
+                    .filter(e => e !== undefined)
+                wrap(newCollatedEvents, newRdg)
+
+                // remove all these events from the existing rdg 
+                for (const event of eventsWithHand) {
+                    const index = rdg.children.indexOf(event)
+                    if (index === -1) continue
+
+                    rdg.children.splice(index, 1)
                 }
-
-                affectedNode.parent = newCollatedEvent
-                otherRdgNode.children.push(newCollatedEvent)
-                appNode.children = [rdgNode, otherRdgNode]
-
-                this.body.children.push(appNode)
             }
         }
     }
 }
+
