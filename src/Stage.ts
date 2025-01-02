c71fc2c679c69a6f893848d1a0774019b66cfbf8
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2025-01-02 15:49:10 +0100

    method for automatically generating edit steps

--- src/Stage.ts
+++ src/Stage.ts
@@ -1,3 +1,5 @@
+import { v4 } from "uuid";
+import { CollationResult } from "./Collator";
 import { Edit, ObjectUsage, Stage } from "./EditorialActions";
 
 /**
@@ -16,6 +18,57 @@ export class StageCreation {
 
     get actions() {
         return [...this.edits, this.basedOn]
+    }
+
+    fillEdits(usingCollation: CollationResult) {
+        if (!('witnesses' in this.basedOn.original)) return
+
+        // inserts: collated events that contain events
+        // belonging to witnesses of the current stage,
+        // but do not contain any events belonging 
+        // to witnesses of the previous stage.
+        for (const collatedEvent of usingCollation.events) {
+            let containedInWitnesses = collatedEvent.wasCollatedFrom.some(rollEvent =>
+                this.created.witnesses.some(witness => witness.hasEvent(rollEvent))
+            )
+
+            let containedInAncestor = collatedEvent.wasCollatedFrom.some(rollEvent => (
+                (this.basedOn.original as Stage).witnesses.some(witness => witness.hasEvent(rollEvent))
+            ))
+
+            if (!containedInWitnesses && containedInAncestor) {
+                // that's a remove
+                this.edits.push({
+                    action: 'delete',
+                    argumentation: {
+                        actor: '#collation-tool',
+                        premises: [`
+                            The event is included in at least one of the witnesses of the assumed 
+                            ancestor, but not in any of the witnesses of the current stage.`]
+                    }, 
+                    certainty: 'true',
+                    type: 'edit',
+                    id: v4(),
+                    contains: [collatedEvent]
+                })
+            }
+            else if (containedInWitnesses && !containedInAncestor) {
+                // that's an insert
+                this.edits.push({
+                    action: 'insert',
+                    argumentation: {
+                        actor: '#collation-tool',
+                        premises: [`
+                            The event is included in at least one of the witnesses of the current 
+                            stage, but not in any of the witnesses of the assumed ancestor.`]
+                    },
+                    certainty: 'true',
+                    type: 'edit',
+                    id: v4(),
+                    contains: [collatedEvent]
+                })
+            }
+        }
     }
 }
 
