d4e564d9651cd08d5f12b578dc0942c97ac450cd
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2025-06-22 22:39:10 +0200

    add simple merge function

--- src/Edit.ts
+++ src/Edit.ts
@@ -1,6 +1,7 @@
+import { v4 } from "uuid";
 import { Person } from "./Edition";
-import { EditorialAssumption, Motivation } from "./EditorialAssumption";
-import { AnySymbol } from "./Symbol";
+import { assign, EditorialAssumption, Motivation } from "./EditorialAssumption";
+import { AnySymbol, dimensionOf, ExpressionType } from "./Symbol";
 import { WithId } from "./WithId";
 
 export const editMotivations = [
@@ -38,3 +39,90 @@ export interface Edit extends WithId {
 export const isEdit = (object: any): object is Edit => {
     return 'insert' in object || 'delete' in object;
 };
+
+const arraysIdentical = <T,>(a: T[], b: T[]) => {
+    let i = a.length;
+    if (i != b.length) return false;
+    while (i--) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+};
+
+const guessMotivation = (edit: Edit): EditMotivation => {
+    const inserts = (edit.insert || [])
+    const deletes = (edit.delete || [])
+
+    const types = [
+        inserts.filter(e => e.type === 'expression').map(e => e.expressionType),
+        deletes.filter(e => e.type === 'expression').map(e => e.expressionType)
+    ]
+
+    if (arraysIdentical<ExpressionType[]>(
+        types, [['SlowCrescendoOn', 'SlowCrescendoOff'], []]
+    )) {
+        return 'additional-accent'
+    }
+    else if (arraysIdentical<ExpressionType[]>(
+        types, [['ForzandoOn', 'ForzandoOff'], []]
+    )) {
+        // TODO: check if the inserts are very close
+        // and return 'short-dynamic-differentation'
+        return 'additional-accent'
+    }
+    else if (types.every(t => t.length > 1) && arraysIdentical<ExpressionType>(
+        types[0],
+        types[1]
+    )) {
+        return 'shift'
+    }
+    else if (types[0].length === 0 && types[1].length === 1) {
+        return 'remove-redundancy'
+    }
+
+    if (inserts.length === 1 && deletes.length === 1) {
+        const insertDim = dimensionOf(inserts[0]).horizontal
+        const deleteDim = dimensionOf(deletes[0]).horizontal
+        
+        if (Math.abs(insertDim.from - deleteDim.from) < 5) {
+            const insertLength = Math.abs(insertDim.to - insertDim.from)
+            const deleteLength = Math.abs(deleteDim.to - deleteDim.from)
+            if (insertLength < deleteLength) {
+                return 'shorten'
+            }
+            else {
+                return 'prolong'
+            }
+        }
+    }
+
+    return 'correct-error'
+}
+
+export const merge = (selection: Edit[]): Edit => {
+    const result = selection[0]
+
+    selection
+        .slice(1)
+        .forEach(edit => {
+            if (result.insert) {
+                result.insert.push(...(edit.insert || []))
+            }
+            else {
+                result.insert = edit.insert
+            }
+
+            if (result.delete) {
+                result.delete.push(...(edit.delete || []))
+            }
+            else {
+                result.delete = edit.delete
+            }
+        })
+
+    return {
+        ...result,
+        id: v4(),
+        motivation: assign('motivationAssignment', guessMotivation(result)),
+    }
+}
